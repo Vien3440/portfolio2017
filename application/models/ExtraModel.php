f7e0cf7b54bbdd8ae78ddb0008edb2b9f426512a
vien34 <vien34@vien34-Vostro-3559>
2017-06-16 09:24:16 +0200

    init model : Course / Extra / Work

---
+++ application/models/ExtraModel.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ExtraModel
+ *
+ * @author vien34
+ */
+class ExtraModel extends CrudModel{
+    //put your code here
+    public function create() {
+        
+    }
+
+    public function delete() {
+        
+    }
+
+    public function getAll() {
+        
+    }
+
+    public function update() {
+        
+    }
+
+}
